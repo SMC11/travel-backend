b01780bf37d752d0498379a969e2ae87748ce78b
SrivalliTalluri <127823664+SrivalliTalluri@users.noreply.github.com>
2023-06-01 05:53:57 +0000

    Added sites changes

--- app/models/site.model.js
+++ app/models/site.model.js
@@ -1,34 +1,29 @@
 module.exports = (sequelize, Sequelize) => {
-    const Site = sequelize.define("site", {
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: Sequelize.STRING(2048),
-        allowNull: false,
-      },
-      duration: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      address: {
-        type: Sequelize.STRING(2048),
-        allowNull: false,
-      },
-      link: {
-        type: Sequelize.STRING(2048),
-        allowNull: false,
-      },
-      startTime: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      photo: {
-        type: Sequelize.BLOB,
-        allowNull: false,
-      },
-    });
-    return Site;
-  };
-  
+  const Site = sequelize.define("site", {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING(2048),
+      allowNull: false,
+    },
+    duration: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: Sequelize.STRING(2048),
+      allowNull: false,
+    },
+    link: {
+      type: Sequelize.STRING(2048),
+      allowNull: false,
+    },
+    photo: {
+      type: Sequelize.STRING(2048),
+      allowNull: false,
+    },
+  });
+  return Site;
+};
